f60052fdf0476727bad8657f1a8bd4deff23d31d
Jos Dehaes <jos.dehaes@gmail.com>
2026-01-06 10:25:07 +0100

    better cleanup after cosmic calamares is done

--- bin/first-time-setup.sh
+++ bin/first-time-setup.sh
@@ -226,6 +226,12 @@ Model=$xkbmodel
 EOF
         chown "$uidgid" "$userhome/.config" "$userhome/.config/kxkbrc"
     fi
+    # we installed mutter temporarily for calamares on cosmic, so remove it after we're finished with calamares
+    if [ -x /usr/bin/cosmic-comp ]; then
+        pacman -Rc --noconfirm mutter || true
+        # also remove any other unneeded dependencies that got installed with mutter
+        pacman -Qdtq | xargs -ro pacman -Rns --noconfirm || true
+    fi
     systemctl disable calamares.service
     usermod -p '*' root
 fi
